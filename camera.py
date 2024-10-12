e7848e23548de1b8159d2ca32e21467ebb5d21f5
promise <ekhosuehipromise@coventry.ac.uk>
2024-10-12 23:39:05 +0100

    MQTT and Firebase settings

--- camera.py
+++ camera.py
@@ -6,3 +6,14 @@ from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials, storage
 from playsound import playsound
+
+# MQTT settings
+MQTT_BROKER = "broker.hivemq.com"
+MQTT_PORT = 1883
+MQTT_TOPIC = "home/camera_motion"
+
+# Firebase settings
+FIREBASE_CREDENTIALS_PATH = "ServiceAccountKey.json"
+FIREBASE_STORAGE_BUCKET = "only-motion-detector.appspot.com"
+# Alarm sound file path
+ALARM_SOUND_PATH = "alarm.mp3" 
